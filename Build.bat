ce7000cebc0587eed053a542eb417d6ce18197f3
Nikos Kastellanos <nkastellanos@gmail.com>
2024-03-17 16:29:02 +0200

    rename NETSTANDARD2_0 projects

--- Build.bat
+++ Build.bat
@@ -1,17 +1,17 @@
-dotnet build Aether.Physics2D.NETSTANDARD2_0.sln /t:restore /p:Configuration=Release
-dotnet build Aether.Physics2D.NETSTANDARD2_0.sln /t:Build /p:Configuration=Release
+dotnet build Aether.Physics2D.sln /t:restore /p:Configuration=Release
+dotnet build Aether.Physics2D.sln /t:Build /p:Configuration=Release
 
 dotnet build Aether.Physics2D.NET40.sln /t:restore /p:Configuration=Release
 dotnet build Aether.Physics2D.NET40.sln /t:Build /p:Configuration=Release
 
-dotnet build Aether.Physics2D.NETSTANDARD2_0.KNI.sln /t:restore /p:Configuration=Release
-dotnet build Aether.Physics2D.NETSTANDARD2_0.KNI.sln /t:Build /p:Configuration=Release
+dotnet build Aether.Physics2D.KNI.sln /t:restore /p:Configuration=Release
+dotnet build Aether.Physics2D.KNI.sln /t:Build /p:Configuration=Release
 
 dotnet build Aether.Physics2D.NET40.KNI.sln /t:restore /p:Configuration=Release
 dotnet build Aether.Physics2D.NET40.KNI.sln /t:Build /p:Configuration=Release
 
-dotnet build Aether.Physics2D.NETSTANDARD2_0.MG.sln /t:restore /p:Configuration=Release
-dotnet build Aether.Physics2D.NETSTANDARD2_0.MG.sln /t:Build /p:Configuration=Release
+dotnet build Aether.Physics2D.MG.sln /t:restore /p:Configuration=Release
+dotnet build Aether.Physics2D.MG.sln /t:Build /p:Configuration=Release
 
 dotnet build Aether.Physics2D.NET40.MG.sln /t:restore /p:Configuration=Release
 dotnet build Aether.Physics2D.NET40.MG.sln /t:Build /p:Configuration=Release
