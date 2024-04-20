cde1f0fab6cc793201397e623413bb404b23c80c
Nikos Kastellanos <nkastellanos@gmail.com>
2024-04-20 10:20:12 +0300

    pack NET.Sdk projects

---
+++ pack.bat
@@ -0,0 +1,5 @@
+dotnet pack Aether.Physics2D.sln /p:Configuration=Release  -o bin\publish
+dotnet pack Aether.Physics2D.KNI.sln /p:Configuration=Release  -o bin\publish
+dotnet pack Aether.Physics2D.MG.sln /p:Configuration=Release  -o bin\publish
+
+@pause
