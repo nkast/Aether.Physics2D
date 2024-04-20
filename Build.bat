1ac2592564d1f499657939e20f3949bea6780831
Nikos Kastellanos <nkastellanos@gmail.com>
2024-04-20 10:18:46 +0300

    replace NET40 projects with NET.Sdk project

--- Build.bat
+++ Build.bat
@@ -1,19 +1,10 @@
 dotnet build Aether.Physics2D.sln /t:restore /p:Configuration=Release
 dotnet build Aether.Physics2D.sln /t:Build /p:Configuration=Release
 
-dotnet build Aether.Physics2D.NET40.sln /t:restore /p:Configuration=Release
-dotnet build Aether.Physics2D.NET40.sln /t:Build /p:Configuration=Release
-
 dotnet build Aether.Physics2D.KNI.sln /t:restore /p:Configuration=Release
 dotnet build Aether.Physics2D.KNI.sln /t:Build /p:Configuration=Release
 
-dotnet build Aether.Physics2D.NET40.KNI.sln /t:restore /p:Configuration=Release
-dotnet build Aether.Physics2D.NET40.KNI.sln /t:Build /p:Configuration=Release
-
 dotnet build Aether.Physics2D.MG.sln /t:restore /p:Configuration=Release
 dotnet build Aether.Physics2D.MG.sln /t:Build /p:Configuration=Release
-
-dotnet build Aether.Physics2D.NET40.MG.sln /t:restore /p:Configuration=Release
-dotnet build Aether.Physics2D.NET40.MG.sln /t:Build /p:Configuration=Release
 
 @pause
