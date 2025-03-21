8f522b5b6d2c1edd427de2692bc627c6d38411c3
Nikos Kastellanos <nkastellanos@gmail.com>
2025-03-21 12:37:14 +0200

    Upgrade libraries to Version 2.2

--- Physics2D.Diagnostics/Diagnostics/Properties/AssemblyInfo.MG.cs
+++ Physics2D.Diagnostics/Diagnostics/Properties/AssemblyInfo.MG.cs
@@ -15,5 +15,5 @@ using System.Reflection;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
